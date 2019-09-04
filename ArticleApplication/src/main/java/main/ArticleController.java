b091af3767d7f1eae53680d1ba44df7b5ec5ae02
Fernando Gontijo <fernando@MacBook-Pro-de-MACBOOK.local>
2019-09-03 23:51:09 -0300

    Estudo Spring Boot

---
+++ ArticleApplication/src/main/java/main/ArticleController.java
@@ -0,0 +1,34 @@
+package main;
+
+
+import model.Article;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ArticleController {
+
+   @Autowired
+    private ArticleService articleService;
+
+
+
+    @RequestMapping("/articles")
+    public List<Article> getAllArticles()
+    {
+
+        return articleService.getAllArticles();
+    }
+
+    @RequestMapping("/articles/{id}")
+    public Article getArticle(@PathVariable String id)
+    {
+        return articleService.getArticle(id);
+    }
+
+
+}
---
+++ ArticleApplication/src/main/java/main/ArticleService.java
@@ -0,0 +1,26 @@
+package main;
+
+import model.Article;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArticleService {
+
+    List<Article> articleList = new ArrayList<Article>(Arrays.asList(
+            new Article("1",  "Article 01",  "Description 01"),
+            new Article( "2", "Article 02", "Description 02"),
+            new Article( "3", "Article 03",  "Description 03")
+            ));
+
+
+    public List<Article> getAllArticles(){
+        return articleList;
+    }
+
+
+    public Article getArticle(String id)
+    {
+        return articleList.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+}
